bd8e020eba3793c848c61b67419cc6d925f919a0
Mariam Nieves <99565269+MariamNieves@users.noreply.github.com>
2022-04-27 08:52:36 -0500

    Update scrollSuave.js

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -1,40 +1,54 @@
 $(document).ready(function () {
+    
+    var scrollBtn = $('.volverArriba')
+
+    
+    window.onscroll = function() {
+        var y = window.scrollY;
+        
+        if (y > 600) {
+            scrollBtn.css('margin-right','0');
+        };
+        if (y < 600) {
+            scrollBtn.css('margin-right','-60px');
+        };
+    };
+
     //movimiento suave de scroll de Inicio y Volver arriba
 
-    document.getElementsByClassName('volverArriba').on('click',function (e) {
+    $('a.volverArriba').on('click',function (e) {
         e.preventDefault();
 
         if ($(window).scrollTop() != 0) {
-        document.getElementsByName('html, body').stop().animate({scrollTop:0}, 1000)
+            $('html, body').stop().animate({scrollTop:0}, 1000)
         }
-    });
+    })
 
     //Demás botones de scroll
-    document.getElementsByClassName('scrollSobremi').on('click',function (e) {
+    $('a.scrollSobremi').on('click',function (e) {
         e.preventDefault();
         var seccionOffsetTop = $($(this).attr('href')).offset().top;
-        document.getElementsByName('html, body').stop().animate({scrollTop:seccionOffsetTop}, 1000)
-    });
+            $('html, body').stop().animate({scrollTop:seccionOffsetTop}, 1000)
+    })
 
-    document.getElementsByClassName('scrollServicios').on('click',function (e) {
+    $('a.scrollServicios').on('click',function (e) {
         e.preventDefault();
         var seccionOffsetTop = $($(this).attr('href')).offset().top;
-        document.getElementsByName('html, body').stop().animate({scrollTop:seccionOffsetTop-200}, 1000)
-    });
+            $('html, body').stop().animate({scrollTop:seccionOffsetTop-200}, 1000)
+    })
+    
+    $('a.scrollContactame').on('click',function (e) {
+        e.preventDefault();
+        var seccionOffsetTop = $($(this).attr('href')).offset().top;
+            $('html, body').stop().animate({scrollTop:seccionOffsetTop-50}, 1000)
+    })
 
-    document.getElementsByClassName('scrollPortafolio').on('click',function (e) {
+    $('a.scroll-contacto').on('click',function (e) {
         e.preventDefault();
         var seccionOffsetTop = $($(this).attr('href')).offset().top;
-        documentgetElementsByName('html, body').stop().animate({scrollTop:seccionOffsetTop-50}, 1000)
-    });
-
-    document.getElementsByClassName('scrollContactame').on('click',function (e) {
-        e.preventDefault();
-        var seccionOffsetTop = $($(this).attr('href')).offset().top;
-        document.getElementsByName('html, body').stop().animate({scrollTop:seccionOffsetTop+85}, 1000)
-    });  
+            $('html, body').stop().animate({scrollTop:seccionOffsetTop+85}, 1000)
+    })
 
 
 
 });
-
