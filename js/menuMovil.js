feda5f69c1fc0ea6ab929583dd6ac03b74af6d33
Mariam Nieves <99565269+MariamNieves@users.noreply.github.com>
2022-04-27 15:00:01 -0500

    Add files via upload

---
+++ js/menuMovil.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    
+    var boton = $('#botonMenu');
+    var desplegable = $('#desplegable');
+    var opciones = $('#desplegable a');
+    var show = 0
+
+    function mostrarOcultar(e) {
+        e.preventDefault
+        if (show == 0) {
+            desplegable.animate({right: '0%'},250);
+            show = 1;
+        } else{
+            desplegable.animate({right: '-30%'},250);
+            show = 0;
+        }
+    }
+
+    function ocultar() {
+        desplegable.animate({right: '-30%'},250);
+            show = 0;
+    }
+
+    opciones.on('click',ocultar);
+
+    boton.on('click', mostrarOcultar);
+
+    
+
+
+
+
+
+
+
+
+
+});
